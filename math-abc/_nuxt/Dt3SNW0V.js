7128a77a156ca2cf72ca9c6e62a97c9c1b1bfdb9
leo <leoding86@msn.com>
2024-04-30 16:23:24 +0800

    create math-abc

---
+++ math-abc/_nuxt/Dt3SNW0V.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DXi4Hjv9.js";import{_ as o,c,b as n,w as a,o as _,d as r}from"./DGaCHYxv.js";const s={};function m(d,f){const t=e;return _(),c("h1",null,[n(t,{to:"/math-abc"},{default:a(()=>[r("Math ABC")]),_:1})])}const l=o(s,[["render",m]]);export{l as default};
